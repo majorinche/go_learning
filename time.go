319c1947a5e5e0943a5ab15db67d08a7e2a69a70
majorinche <594216467@qq.com>
2019-01-12 16:52:34 +0800

    make standard by dep ensure

--- time.go
+++ time.go
@@ -2,9 +2,12 @@ package main
 import (
 	"fmt"
 	"time"
+	"runtime"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	t := time.Now()
 	fmt.Printf("Go launched at %s\n", t.Local())
+	logrus.Infof("Go Version: %s", runtime.Version())
 }
