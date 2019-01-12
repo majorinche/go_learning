9815cc9d96a9481a66291f18b67fa37e9bcafdde
majorinche <594216467@qq.com>
2019-01-12 16:58:07 +0800

    add operator-sdk to try to deep learning the kubernetes operator

--- time.go
+++ time.go
@@ -4,10 +4,12 @@ import (
 	"time"
 	"runtime"
 	"github.com/sirupsen/logrus"
+	sdkVersion "github.com/operator-framework/operator-sdk/version"
 )
 
 func main() {
 	t := time.Now()
 	fmt.Printf("Go launched at %s\n", t.Local())
 	logrus.Infof("Go Version: %s", runtime.Version())
+	logrus.Infof("operator-sdk Version: %v", sdkVersion.Version)
 }
