b0df4cd92a6f46941b30ebd7077138707a005b23
majorinche <594216467@qq.com>
2019-01-28 16:01:49 +0800

    add prometheus default common log package

---
+++ prometheus_log.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/prometheus/common/log"
+)
+
+func main() {
+	log.Infoln("Starting ipmi_exporter mc_20190128")
+	var test *string
+	test = new(string)
+	*test = "majorin"
+	log.Infof("Starting ipmi_exporter mc_20190128: %s", test)
+	log.Infof("Starting ipmi_exporter mc_20190128: %s", *test)
+}
