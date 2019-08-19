f277da2ac37ea28671e7bc3b5d9c510a6c822b28
majorinche <594216467@qq.com>
2019-08-19 11:16:18 +0800

    增加注释

--- ioutil.go
+++ ioutil.go
@@ -4,22 +4,22 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"os/exec"
+	//"os/exec"
 )
 
 var (
 	configFile = flag.String(
-		"config.file", "ipmi.yml", //only ipmi.yml is the real value of configFile
+		"config.file", "ipmi.yml", //
 		"Path to configuration file.",
 	)
 )
 
 func main() {
-	tt, _ := ioutil.ReadFile(*configFile) // return of flag.String is a memory address, so need star to retrieve real value, whichi should be ipmi.yml here
-	fmt.Printf("%s", tt)                  //here will print out the content in the file, ipmi.yml
-	args := []string{}
-	fmt.Printf("%s", args) //here will print out the content in the file, ipmi.yml
-	args = append(args, "-h 192.168.2.130 -u admin -p admin")
+	tt, _ := ioutil.ReadFile(*configFile)                     // 因为flag.string的返回值是指针，所以这里需要*号,ioutil.ReadFile直接读取文件的内容
+	fmt.Printf("%s", tt)                                      //here will print out the content in the file, ipmi.yml
+	args := []string{}                                        //前面是数组，后面看似是结构体，到底是啥？
+	fmt.Printf("%s", args)                                    //here will print out the content in the file, ipmi.yml
+	args = append(args, "-h 192.168.2.130 -u admin -p admin") //如果前面定义过args，这里再使用，就可以直接使用等于号了
 	fqcmd := "ipmimonitoring"
 	out, err := exec.Command(fqcmd, args...).CombinedOutput()
 	fmt.Printf("%s %s", out, err)
