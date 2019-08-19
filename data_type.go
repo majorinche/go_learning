ead322d33ad7f71826166681136fc3e69c8ed072
majorinche <594216467@qq.com>
2019-08-19 10:47:20 +0800

    加入内存地址&b赋值给，函数的接收参数为*bool的部分

--- data_type.go
+++ data_type.go
@@ -1,8 +1,16 @@
 package main
-import("fmt")
+
+import (
+	"fmt"
+)
 
 func main() {
 	var b bool
 	fmt.Println(b)
 	fmt.Println(&b)
+	test_tt(&b) //&b就代表你需要传递参数的函数必须是*bool的形式，才可以使用&b
+}
+
+func test_tt(ccc *bool) {
+	fmt.Print(ccc)
 }
