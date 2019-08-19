92d7fe5a5e1f858f245b36865e1b0dee169ab956
majorinche <594216467@qq.com>
2019-08-19 11:08:13 +0800

    增加iota的注释，常量环境下自增

--- iota.go
+++ iota.go
@@ -1,18 +1,23 @@
+/*
+	20190819 - iota可以保证常量环境下，不需要定义变量的值，也能自动计数，用在什么场景再看
+
+*/
+
 package main
 
 import "fmt"
 
 func main() {
-    const (
-            a = iota   //0
-            b          //1
-            c          //2
-            d = "ha"   //独立值，iota += 1
-            e          //"ha"   iota += 1
-            f = 100    //iota +=1
-            g          //100  iota +=1
-            h = iota   //7,恢复计数
-            i          //8
-    )
-    fmt.Println(a,b,c,d,e,f,g,h,i)
+	const (
+		a = iota //0
+		b        //1
+		c        //2
+		d = "ha" //独立值，iota += 1
+		e        //"ha"   iota += 1
+		f = 100  //iota +=1
+		g        //100  iota +=1
+		h = iota //7,恢复计数
+		i        //8
+	)
+	fmt.Println(a, b, c, d, e, f, g, h, i)
 }
