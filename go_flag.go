087e92bd430bd3d1655d8e732dd12fcbf62bda7f
majorinche <594216467@qq.com>
2019-08-19 11:04:23 +0800

    增加flag包的命令行参数部分注释

--- go_flag.go
+++ go_flag.go
@@ -1,3 +1,11 @@
+/*
+	20190819 - flag是解析命令含参数的包
+	使用方法：
+F:\go\src\github.com\majorinche\go_learning>go run go_flag.go -s ccc -d ffff
+value of s: 0xc000030210
+value of d: ffff
+
+*/
 package main
 
 import (
@@ -6,9 +14,9 @@ import (
 )
 
 func main() {
-	aa := flag.String("s", "hw", "s h text") //first is flag,second is value, third maybe help message
-	bb := flag.String("d", "dddd", "s h text")
+	aa := flag.String("s", "hw", "s h text")   //first is flag,second is value, third maybe help message
+	bb := flag.String("d", "dddd", "s h text") //第二个是一个默认值
 	flag.Parse()
-	fmt.Println("value of s:", aa)  // output is: value of s: 0xc00000e200, so start+variable has two meaning: 1, declare a memory address variable, 2, retrieve a real value from memory address
+	fmt.Println("value of s:", aa)  // 默认flag的返回值是内存地址，即指针，如果需要返回实际值，需要加*号
 	fmt.Println("value of d:", *bb) // output is: value of d: dddd
 }
