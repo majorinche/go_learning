85560da6f0bcc9bba2feb8324f944828ad01f8d7
majorinche <594216467@qq.com>
2020-08-25 11:13:39 +0800

    update

---
+++ k8s/README.md
@@ -0,0 +1,8 @@
+mkdir -p /root/go/src/k8s.io
+git clone https://github.com/kubernetes/apimachinery
+
+mkdir -p /root/go/src/github.com/gogo
+git clone https://github.com/gogo/protobuf
+
+mkdir -p /root/go/src/github.com/google/
+git clone https://github.com/google/gofuzz
---
+++ k8s/connect_api.go
@@ -0,0 +1,19 @@
+package connect_api
+
+import (
+	"fmt"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	// uses the current context in kubeconfig
+	// path-to-kubeconfig -- for example, /root/.kube/config
+	config, _ := clientcmd.BuildConfigFromFlags("", "/root/.kube/config")
+	// creates the clientset
+	clientset, _ := kubernetes.NewForConfig(config)
+	// access the API to list pods
+	pods, _ := clientset.CoreV1().Pods("").List(v1.ListOptions{})
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+}
