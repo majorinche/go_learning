0ef44ba2f5444840b937a61d19b6db2ea27d7456
majorinche <594216467@qq.com>
2021-01-20 14:22:00 +0800

    进一步理解空接口通道和defer特性

--- dns-check.go
+++ dns-check.go
@@ -1,3 +1,8 @@
+/*
+	20210120 - defer用不用，对接口还是有影响，但是这个影响是否有什么实际价值，不是很确定
+
+*/
+
 package main
 
 import (
@@ -34,20 +39,21 @@ func main() {
 		sum int64 = 0
 	)
 
-	go func() {
+	go func() { //这里的go表示以并发方式，调用匿名函数func(), 我理解是会一直调用，而不是侧重于并发，而是侧重于连续性，从某个角度看，也的确是并发的
 		time.Sleep(time.Second * time.Duration(duration))
 		exit <- true
 	}()
 endD:
 	for {
 		select {
-		case pool <- nil:
-			fmt.Println(len(pool)) //有意思，我们不一定知道通道里面是什么，但是至少知道，用了一次，就会减少一次，通道就是go版的kafka
+		case pool <- nil: //这里最终会赋值10次nil到pool中，因为pool的长度是10
+			//			fmt.Println(len(pool)) //有意思，我们不一定知道通道里面是什么，但是至少知道，用了一次，就会减少一次，通道就是go版的kafka
 			go func() {
 				defer func() {
-					//		fmt.Println(len(pool))
-					<-pool
+					//fmt.Println(len(pool))
+					<-pool //这里就是把通道里面的东西用了一次，不过为什么用defer没有想通
 				}()
+				//<-pool
 				resolver := &net.Resolver{}
 				now := time.Now()
 				_, err := resolver.LookupIPAddr(context.Background(), host)
@@ -62,7 +68,7 @@ endD:
 				if limit > 0 && use >= limit {
 					timeoutCount++
 				}
-				atomic.AddInt64(&count, 1)
+				atomic.AddInt64(&count, 1) //这里居然用到原子性，后期再看，有意思
 				if err != nil {
 					fmt.Println(err.Error())
 					atomic.AddInt64(&errCount, 1)
